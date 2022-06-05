6ce2fb320e14df766e7b25207f54e04323db9f49
burdeniucmarin <106924186+burdeniucmarin@users.noreply.github.com>
2022-06-05 19:48:55 +0300

    Add files via upload

---
+++ Prototype_Pattern/Catalog_TI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Pattern
+{
+    class Catalog_TI : Catalog
+    {
+        public List<string> obiecte = new List<string>();
+        public List<string> studenti = new List<string>();
+        public int nr_ob;
+        public int nr_st;
+        string[,] data = new string[50, 50];
+        public override void Get_Obiecte()
+        {
+            Console.Write("Nr de obiecte > ");
+            nr_ob = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < nr_ob; i++)
+            {
+                Console.Write($"Obiectul [{i}] > ");
+                string ob = new string(Console.ReadLine());
+                obiecte.Add(ob);
+            }
+        }
+        public override void Get_Studenti()
+        {
+            Console.Write("Nr de studenti > ");
+            nr_st = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < nr_st; i++)
+            {
+                Console.Write($"Studentul [{i}] > ");
+                string ob = new string(Console.ReadLine());
+                studenti.Add(ob);
+            }
+        }
+        
+        public override void Creare()
+        {
+            data[0, 0] = "      ";
+            for (int i = 1; i < nr_ob; i++)
+            {
+                for (int j = 1; j < nr_st; j++)
+                {
+                    data[i,j] = "-";
+                }
+            }
+
+            for(int i = 1; i < nr_ob; i++)
+            {
+                data[1, i] = obiecte[i-1];
+            }
+            for (int i = 1; i < nr_st; i++)
+            {
+                data[i, 1] = studenti[i-1];
+            }
+
+            for (int i = 0; i < nr_ob; i++)
+            {
+                for (int j = 0; j < nr_st; j++)
+                {
+                    Console.Write(" " + data[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public override Catalog Clone()
+        {
+            Catalog_TI copy = new Catalog_TI();
+            copy.nr_ob = this.nr_ob;
+            copy.obiecte = this.obiecte;
+            return copy;
+        }
+    }
+}
---
+++ Prototype_Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Catalog_TI catalog_ti193 = new Catalog_TI();
+            catalog_ti193.Get_Obiecte();
+            catalog_ti193.Get_Studenti();
+            catalog_ti193.Creare();
+
+            catalog_ti193.obiecte[0] = "test";
+
+            Catalog_TI catalog_ti192 = (Catalog_TI)catalog_ti193.Clone();
+            catalog_ti192.Get_Studenti();
+            catalog_ti192.Creare();
+            Console.WriteLine(catalog_ti192.obiecte[0]);
+        }
+    }
+}
